e4c3a0364fdb645f89f2f0e1ff6832483aed593b
mlimberg <limbergmike@gmail.com>
2016-10-11 08:44:39 -0600

    update javascript and add in new css with button layouts

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ var userGuess;
 var lastGuess = document.querySelector('.last-guess');
 var guessButton = document.querySelector('.guess');
 var randomNumber = (Math.ceil(Math.random() * 100));
+var response = document.querySelector('.response');
+
 
 function displayGuess(){
   lastGuess.innerText = userGuess;
@@ -19,17 +21,24 @@ function getUserGuess() {
    console.log(randomNumber)
   });
 
+
+//break function below into two, one to check that it's a valid input, other to compare//
   function compareGuess() {
     if (userGuess > 100) {
       console.log("Please enter a number between 1 and 100");
+      response.innerText = "Please enter a number between 1 and 100";
     } else if (userGuess > randomNumber) {
       console.log("That is too high");
+      response.innerText = "That is too high";
     } else if (userGuess < randomNumber) {
-      console.log("That is too low")
+      console.log("That is too low");
+      response.innerText = "That is too low";
     } else {
       console.log("You guessed it!")
+      response.innerText = "You guessed it!";
     }
   };
+
 
 // function compareGuess() {
 //   if (userGuess > randomNumber) {
