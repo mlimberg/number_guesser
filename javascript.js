e6574409fc9a8e0c4f7573c95e15e97d60e65b03
mlimberg <limbergmike@gmail.com>
2016-10-12 23:51:48 -0600

    add js to increase min and max values by 10 when user guesses correctly, fixed a submit button bug and added scroll bar for long submissions

--- javascript.js
+++ javascript.js
@@ -2,11 +2,6 @@
 var min = 1;
 var max = 100;
 
-// creating this function for use in the reset button so that it generates a new random number.
-function generateRandomNumber() {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
 var submitButton = document.querySelector(".changeminmax")
 var minValueField = document.querySelector("#min-value")
 var maxValueField = document.querySelector("#max-value")
@@ -37,26 +32,24 @@ var clearButton = document.querySelector('.clear');
 var userInputString = document.getElementById('main-input');
 
 var rangeText = document.querySelector('.min-max-text');
+// creating this function for use in the reset button so that it generates a new random number.
+function generateRandomNumber() {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-
+//list the range the user must guess between//
   rangeText.innerText = `Enter a number between ${min} and ${max}`
-
-
 
 //function to simplify getting parsed integer//
 function inputAsNumber() {
   return  parseInt(document.getElementById('main-input').value)
 };
 
-
-
-
 //function to display the "your last guess was" text and the user input as a string - need to set character limit to avoid layout issues if user puts too many characters//
 function displayInputString() {
   guessSection.innerText = document.getElementById('main-input').value;
   yourLastGuessWas.innerText = "Your last guess was";
 };
-
 
 // //function to compare input to random number and return a response accordingly//
 function compareAndInform() {
@@ -71,18 +64,35 @@ function compareAndInform() {
     resultSection.innerText = "That is too high";
   } else if (inputAsNumber() === randomNumber) {
     resultSection.innerText = "You guessed it!"
+    increaseValueOnWin();
+    console.log(randomNumber);
   } else {
     resultSection.innerText = `You need to guess a NUMBER between ${min} and ${max}`;
   }
 };
 
+//function to increase the min and max value by 10 if the user gets it right. requires updating random number and displayed text as well//
+function increaseValueOnWin() {
+  if (inputAsNumber() === randomNumber) {
+      min = min + 10;
+      max = max + 10;
+      randomNumber = generateRandomNumber();
+      updateMinMaxText();
+    }
+  };
+
 //event listener to run functions when button is  clicked that display the user's input and give them feedback based on the input//
 
+//clicking guess button will display the user's guess and compare it to the random number and spit a response//
 guessButton.addEventListener('click', function() {
     console.log(randomNumber);
     displayInputString();
     compareAndInform();
+    increaseValueOnWin();
+
 });
+
+//allows user to press enter to submit answer//
 mainInputField.addEventListener('keypress', function(e) {
   console.log(e)
   if (e.keyCode===13){
@@ -91,7 +101,8 @@ mainInputField.addEventListener('keypress', function(e) {
     compareAndInform();
   }
 });
-//refreshing the page on click to set a new random number and clear the input//
+
+//resetting the page on click to set a new random number and clear the input//
 function resetPage() {
   randomNumber = generateRandomNumber();
   mainInputField.value = "";
@@ -105,12 +116,14 @@ function resetPage() {
   console.log(randomNumber);
 };
 
+//resets the page and disables buttons//
 resetButton.addEventListener('click', function(){
   resetPage();
   resetButton.disabled = true;
   submitButton.disabled = true;
 });
 
+//placing content in the input field enables buttons for use//
 mainInputField.addEventListener('keyup', function(){
   if (userInputString.value != "") {
     guessButton.disabled = false;
@@ -123,6 +136,7 @@ mainInputField.addEventListener('keyup', function(){
   }
 });
 
+//content in the min value AND max value field will enable the submit button//
 minValueField.addEventListener('keyup', function(){
   if (minValueField.value != "" && maxValueField.value != "") {
     submitButton.disabled = false;
@@ -139,8 +153,6 @@ maxValueField.addEventListener('keyup', function(){
   }
 });
 
-// defining main input field as a variable so we can call it in a function and addEventListener
-
 // defining a function as clearing the input field and replacing it with nothing
 function clearInputField() {
   mainInputField.value = "";
@@ -152,16 +164,21 @@ clearButton.addEventListener('click', function(){
   clearButton.disabled = true;
 });
 
+//updates min and max variables to user preference//
 function updateMinMax() {
   min = parseInt(minValueField.value);
   max = parseInt(maxValueField.value);
-}
+};
+
 function updateMinMaxText () {
   rangeText.innerText = `Enter a number between ${min} and ${max}`
-}
+};
+
+//clicking the submit button updates min and max based on user preference and clears page content//
 submitButton.addEventListener('click', function(){
   updateMinMax();
   resetPage();
   updateMinMaxText();
+  submitButton.disabled = true;  //ADDED//
   // displayRange();
 })
